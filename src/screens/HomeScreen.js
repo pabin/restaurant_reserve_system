186c1ba8503a02d6e5389f6a0817e6949bbb6d67
Pabin R Luitel <pabin2050@gmail.com>
2019-12-12 23:09:02 +0545

    implement modal and divide view

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,8 +7,10 @@ import {
   StyleSheet,
   StatusBar,
   TouchableHighlight,
-  ImageBackground,
   Image,
+  ScrollView,
+  Modal,
+  TextInput,
 } from 'react-native';
 
 class HomeScreen extends Component {
@@ -16,40 +18,146 @@ class HomeScreen extends Component {
       super(props)
 
       this.state = {
-        check_me: true
+        modalVisible: false,
+        quantity: '',
+        userName: '',
       }
+    }
+
+    setModalVisible(visible) {
+      this.setState({modalVisible: visible});
     }
 
     render() {
       console.log("Hi there how are you mate...");
       return (
         <Fragment>
-          <View style={styles.container}>
-            <Text style={styles.textContainer}>
-              QuizSewa
-            </Text>
+            <View style={styles.container}>
 
-            <TouchableHighlight style ={styles.roundedButton}>
-              <Text style={styles.buttonText}>Play Now</Text>
-            </TouchableHighlight>
+              <Modal
+                animationType="fade"
+                transparent={true}
+                visible={this.state.modalVisible}
+                onRequestClose={() => {
+                  Alert.alert('Modal has been closed.');
+                }}>
+                <View style={styles.modalContainer}>
+                  <View style={styles.input} >
+                    <Text style={styles.text}>Name</Text>
+                      <TextInput
+                          style={styles.textInput}
+                          autoCapitalize="none"
+                          onChangeText={quantity => this.setState({quantity})}
+                          autoCorrect={false}
+                          keyboardType="email-address"
+                          returnKeyType="next"
+                          multiline={true}
+                          numberOfLines={4}
+                      />
+                  </View>
 
-            <TouchableHighlight style ={styles.roundedButton}>
-              <Text style={styles.buttonText}>Lok Sewa Quiz</Text>
-            </TouchableHighlight>
+                  <View style={styles.input} >
+                    <Text style={styles.text}>Quantity</Text>
+                      <TextInput
+                          style={styles.textInput}
+                          autoCapitalize="none"
+                          onChangeText={userName => this.setState({userName})}
+                          autoCorrect={false}
+                          keyboardType="email-address"
+                          returnKeyType="next"
+                          multiline={true}
+                          numberOfLines={4}
+                      />
+                  </View>
 
-            <TouchableHighlight style ={styles.roundedButton}>
-              <Text style={styles.buttonText}>Leader Board</Text>
-            </TouchableHighlight>
+                  <View style={styles.input}>
+                    <TouchableHighlight
+                      style={styles.addUser}
+                      onPress={() => {
+                        this.setModalVisible(!this.state.modalVisible);
+                      }}>
+                      <Text style={styles.text}>Add</Text>
+                    </TouchableHighlight>
+                  </View>
+                </View>
+              </Modal>
 
-            <TouchableHighlight style ={styles.roundedButton}>
-              <Text style={styles.buttonText}>Setting</Text>
-            </TouchableHighlight>
+              <View style={styles.userPanel}>
+                <ScrollView>
+                  <TouchableHighlight style={styles.user}>
+                    <Text style={styles.text}>User One</Text>
+                  </TouchableHighlight>
 
-            <TouchableHighlight style ={styles.roundedButton}>
-              <Text style={styles.buttonText}>Profile</Text>
-            </TouchableHighlight>
+                  <TouchableHighlight style={styles.user}>
+                    <Text style={styles.text}>User Two</Text>
+                  </TouchableHighlight>
 
-          </View>
+                  <TouchableHighlight style={styles.user}>
+                    <Text>User Three</Text>
+                  </TouchableHighlight>
+
+                  <TouchableHighlight style={styles.user}>
+                    <Text>User Four</Text>
+                  </TouchableHighlight>
+
+                  <TouchableHighlight
+                    style={styles.addUser}
+                    onPress={() => {
+                      this.setModalVisible(true);
+                    }}>
+                    <Text>Add +</Text>
+                  </TouchableHighlight>
+
+                </ScrollView>
+              </View>
+
+              <View style={styles.tablePanel}>
+                <View style={styles.singleTableRow}>
+                  <View style={styles.table}>
+                    <Text style={styles.text}>Table 1A</Text>
+                  </View>
+
+                  <View style={styles.table}>
+                    <Text style={styles.text}>Table 1B</Text>
+                  </View>
+
+                  <View style={styles.table}>
+                    <Text>Table 1C</Text>
+                  </View>
+                </View>
+
+                <View style={styles.singleTableRow}>
+                  <View style={styles.table}>
+                    <Text>Table 2A</Text>
+                  </View>
+
+                  <View style={styles.table}>
+                    <Text>Table 2B</Text>
+                  </View>
+
+                  <View style={styles.table}>
+                    <Text>Table 2C</Text>
+                  </View>
+                </View>
+
+                <View style={styles.singleTableRow}>
+                  <View style={styles.table}>
+                    <Text>Table 3A</Text>
+                  </View>
+
+                  <View style={styles.table}>
+                    <Text>Table 3B</Text>
+                  </View>
+
+                  <View style={styles.table}>
+                    <Text>Table 3C</Text>
+                  </View>
+                </View>
+
+              </View>
+
+            </View>
+
         </Fragment>
       )
     }
@@ -61,33 +169,85 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
+    flexDirection: 'row',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 
-  textContainer: {
-    fontSize: 38,
-    padding: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    fontFamily: 'Acme',
+  userPanel: {
+    flex: 1,
+    backgroundColor: 'green',
   },
 
-  roundedButton: {
-    backgroundColor: "#1e824c",
+  tablePanel : {
+    flex: 5,
+    flexDirection: 'row',
+    alignItems: 'stretch',
+  },
+
+  user: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: "orange",
     elevation: 5,
     alignItems:'center',
     justifyContent:'center',
     borderRadius:20,
-    marginVertical: 8,
   },
 
-  buttonText: {
-    padding: 8,
-    fontSize: 24,
-    fontWeight: 'bold',
-    fontFamily: 'Roboto',
-    color: '#FFFFFF'
+  addUser: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: "red",
+    elevation: 5,
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:20,
+  },
 
-  }
+  table: {
+    padding: 50,
+    margin: 20,
+    backgroundColor: "orange",
+    alignItems:'center',
+    justifyContent:'center',
+    borderRadius:10,
+  },
+
+  singleTableRow: {
+    flex: 1,
+  },
+
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+
+  modalContainer: {
+    flex: 1,
+    marginHorizontal: 350,
+    marginVertical: 250,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius:20,
+    borderWidth: 2,
+    borderColor: '#000',
+    elevation: 10,
+  },
+
+  input: {
+    flex: 1,
+    alignSelf: 'stretch',
+    margin: 10,
+    padding: 10,
+  },
+
+  textInput: {
+    marginBottom: 10,
+    padding: 10,
+    color: "#fff",
+    borderWidth: 2,
+    borderColor: '#78909C',
+    color: 'black'
+  },
 })
